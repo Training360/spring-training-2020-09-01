c34540ae6cf2c24b1d00bb3cfb3ec4890574e674
iviczian <viczian.istvan@gmail.com>
2020-09-02 14:27:27 +0200

    Status és exception

--- employees/src/main/java/employees/EmployeesController.java
+++ employees/src/main/java/employees/EmployeesController.java
@@ -2,10 +2,16 @@ package employees;
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.zalando.problem.Problem;
+import org.zalando.problem.Status;
 
 import javax.websocket.server.PathParam;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,6 +29,7 @@ public class EmployeesController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public EmployeeDto createEmployee(
             @RequestBody CreateEmployeeCommand command) {
         return employeesService.createEmployee(command);
@@ -36,6 +43,7 @@ public class EmployeesController {
     }
 
     @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteEmployee(@PathVariable("id") long id) {
         employeesService.deleteEmployee(id);
     }
@@ -43,6 +51,25 @@ public class EmployeesController {
     @GetMapping("{id}")
     public EmployeeDto findEmployeeById(@PathVariable("id") long id) {
         return employeesService.findEmployeeById(id);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+//    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Problem> handleIllegalArgumentException(IllegalArgumentException iae) {
+        System.out.println("Error: " + iae.getMessage());
+
+        var problem =
+                Problem.builder()
+                .withType(URI.create("employees/not-found"))
+                .withTitle("Employee not found")
+                .withStatus(Status.NOT_FOUND)
+                .withDetail(iae.getMessage())
+                .build();
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_PROBLEM_JSON)
+                .body(problem);
     }
 
 }
