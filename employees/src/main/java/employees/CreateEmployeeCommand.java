4bba8a3e7ddc38eff6a4cb98a0e53e390d57ad50
iviczian <viczian.istvan@gmail.com>
2020-09-02 15:51:51 +0200

    Swagger

--- employees/src/main/java/employees/CreateEmployeeCommand.java
+++ employees/src/main/java/employees/CreateEmployeeCommand.java
@@ -1,9 +1,12 @@
 package employees;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class CreateEmployeeCommand {
 
+    @Schema(description = "name of the new employee",
+    example = "Sample Joe")
     private String name;
 }
