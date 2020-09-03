ba82300265958a03893678c34efbe430b2ac0e90
iviczian <viczian.istvan@gmail.com>
2020-09-03 14:37:46 +0200

    Tyrus

--- employees-websocket-client/src/main/java/employees/EmployeesWebsocketClientApplication.java
+++ employees-websocket-client/src/main/java/employees/EmployeesWebsocketClientApplication.java
@@ -1,13 +1,28 @@
 package employees;
 
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
-public class EmployeesWebsocketClientApplication {
+@AllArgsConstructor
+public class EmployeesWebsocketClientApplication implements CommandLineRunner {
+
+	private EmployeesGateway employeesGateway;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeesWebsocketClientApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		employeesGateway.subscribe();
+
+		var scanner = new Scanner(System.in);
+		scanner.nextLine();
+
+	}
 }
