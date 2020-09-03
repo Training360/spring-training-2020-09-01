cdbc4da5c2ee6839c1ce19345e4e707e7ad03159
iviczian <viczian.istvan@gmail.com>
2020-09-03 11:10:52 +0200

    Xml személyre szabása

--- employees/src/main/java/employees/EmployeeDto.java
+++ employees/src/main/java/employees/EmployeeDto.java
@@ -4,14 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class EmployeeDto {
 
+    @XmlAttribute
     private Long id;
 
     private String name;
--- employees/src/main/java/employees/EmployeesDto.java
+++ employees/src/main/java/employees/EmployeesDto.java
@@ -4,14 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.List;
 
 @Data
-@XmlRootElement
+@XmlRootElement(name = "employees")
 @AllArgsConstructor
 @NoArgsConstructor
+@XmlAccessorType(XmlAccessType.FIELD)
 public class EmployeesDto {
 
+    @XmlElement(name = "employee")
     private List<EmployeeDto> employees;
 }
